4e57d591cfcef801e2bce6063000a8a13cabe8f8
CRenauro <crenauro@optimum.net>
2021-03-06 16:37:22 -0500

    updated api and store routes

--- db/store.js
+++ db/store.js
@@ -9,47 +9,49 @@ uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
-module.exports = function(app){
+// module.exports = function(app){
     
-    app.get('/api/notes', function (req,res) {
-        fs.readFile('db/db.json', 'utf8', function(error,data) {
-            res.json(JSON.parse(data));
-        });
-    });
+//     store.get('/api/notes', function (req,res) {
+//         fs.readFile('db/db.json', 'utf8', function(error,data) {
+//             res.json(JSON.parse(data));
+//         });
+//     });
 
-        app.post('/api/notes', function (req,res) {
-        var newNote = req.body;
-        newNote.id = uuidv4();
-        fs.readFile('db/db.json', 'utf8', function(error,data) {
-            var data = json.parse(data);
-            data.push(newNote);
-            fs.writeFile('db/db.json', json.stringify(data), function(error) {
-                if (error)
-                    throw error;
-                    console.log("successful");
-            })
+//         store.post('/api/notes', function (req,res) {
+//         var newNote = req.body;
+//         newNote.id = uuidv4();
+//         fs.readFile('db/db.json', 'utf8', function(error,data) {
+//             var data = json.parse(data);
+//             data.push(newNote);
+//             fs.writeFile('db/db.json', json.stringify(data), function(error) {
+//                 if (error)
+//                     throw error;
+//                     console.log("successful");
+//             })
 
-        });
-        res.json(newNote);
+//         });
+//         res.json(newNote);
 
-        });
+//         });
 
-        app.delete('/api/notes/:id', function (req, res) {
-            fs.readFile('db/db.json', 'utf8', function(error,data) {
-                var noteId = req.params.id;
-                var noteData = json.parse(data);
-                noteData = noteData.filter(function(app) {
-                    if (noteId != note.id) {
-                        return true;
-                    }  else {
-                        return false;
-                    };
-                });
-                fs.writeFile('db/db.json', json.stringify(noteData), function(error) {
-                    if (error)
-                    throw error;
-                    res.end(console.log("deleted"));
-                })
-            });
-        });
-};
+//         store.delete('/api/notes/:id', function (req, res) {
+//             fs.readFile('db/db.json', 'utf8', function(error,data) {
+//                 var noteId = req.params.id;
+//                 var noteData = json.parse(data);
+//                 noteData = noteData.filter(function(store) {
+//                     if (noteId != note.id) {
+//                         return true;
+//                     }  else {
+//                         return false;
+//                     };
+//                 });
+//                 fs.writeFile('db/db.json', json.stringify(noteData), function(error) {
+//                     if (error)
+//                     throw error;
+//                     res.end(console.log("deleted"));
+//                 })
+//             });
+//         });
+// };
+
+// module.exports = new Store();
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,36 +1,68 @@
 // import items needed
 const router = require('express').Router();
-const store = require('../db/store')
+const store = require('../db/store');
 const fetch = require("node-fetch");
+const fs = require("fs");
 
 // make a GET request with all notes from the database
 
+// module.exports = function(router){
+
+// };
+
 router.get('/notes', (req, res) => {
-  fetch
-    .then((notes) => {
-        return res.json(notes);
-    })
-    .catch((err) => res.status(500).json(err));
-})
+  fs.readFile('db/db.json', 'utf8', function(error,data) {
+    res.json(JSON.parse(data));
+  });
+});
 
-// create a post request 
-router.post('/notes', (req, res) => {
-  fetch
-    .then((notes) => {
-      return res.json(notes);
-    })
-    .catch((err) => res.status(500).json(err));
-})
+router.post('/api/notes', (req,res) => {
+  fs.readFile('db/db.json', 'utf8', function(error,data) {
+    var newNote = req.body;
+    newNote.id = uuidv4();
+    var data = json.parse(data);
+      data.push(newNote);
+      fs.writeFile('db/db.json', json.stringify(data), function(error) {
+          if (error)
+          throw error;
+          console.log("successful");
+      })
+    });
+    res.json(newNote);
+  });
+
+router.delete('/api/notes/:id', function (req, res) {
+  fs.readFile('db/db.json', 'utf8', function(error,data) {
+      var noteId = req.params.id;
+      var noteData = json.parse(data);
+      noteData = noteData.filter(function(store) {
+          if (noteId != note.id) {
+              return true;
+          }  else {
+              return false;
+          };
+      });
+      fs.writeFile('db/db.json', json.stringify(noteData), function(error) {
+          if (error)
+          throw error;
+          res.end(console.log("deleted"));
+      })
+  });
+});
 
 
-// create a delete request
-router.delete('/notes', (req, res) => {
-  fetch
-    .then((notes) => {
-      return res.json(notes);
-    })
-    .catch((err) => res.status(500).json(err));
-})
+// // create a post request 
+
+
+
+// // create a delete request
+// router.delete('/notes', (req, res) => {
+//   fetch
+//     .then((notes) => {
+//       return res.json(notes);
+//     })
+//     .catch((err) => res.status(500).json(err));
+// })
 
 
 
